f8c606a5dffc63f86871e3cdbdebdb87b5a3ae47
Mardoyan-M <job.mardoyan@gmail.com>
2024-11-11 14:55:19 +0300

    nine commit

--- 02_lesson/lesson_2_task_2.py
+++
@@ -1,8 +0,0 @@
-def is_year_leap(year):
-    if (year % 4 == 0):
-        print("Год: " + str(year) + " True")
-    else:
-        print("Год: " + str(year) + " False")
-
-
-is_year_leap(int(input("Какой год проверить?: ")))
