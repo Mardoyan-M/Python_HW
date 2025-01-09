a924bf43a29c0b136ed2518dfda9eaf6ff42915e
Mardoyan-M <job.mardoyan@gmail.com>
2025-01-09 17:27:21 +0300

    seventeen commit

--- 06K_lesson/test_01_form.py
+++ 06K_lesson/test_01_form.py
@@ -1,78 +1,37 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-
-driver = webdriver.Chrome(
-    service=ChromeService(ChromeDriverManager().install()))
 
 
-driver.get(
-    "https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+def test_form():
 
-waiter = WebDriverWait(driver, 10)
+    driver = webdriver.Chrome()
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
-first_name = driver.find_element(By.CSS_SELECTOR, '[name="first-name"]')
-first_name.send_keys("Иван")
+    form_data = {
+        "first-name": "Иван",
+        "last-name": "Петров",
+        "address": "Ленина, 55-3",
+        "e-mail": "test@skypro.com",
+        "phone": "+7985899998787",
+        "city": "Москва",
+        "country": "Россия",
+        "job-position": "QA",
+        "company": "SkyPro"
+    }
 
-last_name = driver.find_element(By.CSS_SELECTOR, '[name="last-name"]')
-last_name.send_keys("Петров")
+    for field, value in form_data.items():
+        driver.find_element(By.CSS_SELECTOR,
+                            f"[name={field}]").send_keys(value)
 
-address = driver.find_element(By.CSS_SELECTOR, '[name="address"]')
-address.send_keys("Ленина, 55-3")
+    driver.find_element(By.CSS_SELECTOR,
+                        "button[type=submit]").click()
 
-email = driver.find_element(By.CSS_SELECTOR, '[name="e-mail"]')
-email.send_keys("test@skypro.com")
+    for field_id in list(form_data.keys()):
+        assert ("alert-success" in driver.find_element(
+            By.ID, field_id).get_attribute("class"))
 
-phone = driver.find_element(By.CSS_SELECTOR, '[name="phone"]')
-phone.send_keys("+7985899998787")
+    assert ("alert-danger" in driver.find_element(
+        By.ID, 'zip-code').get_attribute("class"))
 
-city = driver.find_element(By.CSS_SELECTOR, '[name="city"]')
-city.send_keys("Москва")
-
-country = driver.find_element(By.CSS_SELECTOR, '[name="country"]')
-country.send_keys("Россия")
-
-job = driver.find_element(By.CSS_SELECTOR, '[name="job-position"]')
-job.send_keys("QA")
-
-company = driver.find_element(By.CSS_SELECTOR, '[name="company"]')
-company.send_keys("QA")
-
-driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-
-waiter = WebDriverWait(driver, 20)
-
-assert "danger" in driver.find_element(
-    By.ID, "zip-code").get_attribute("class")
-
-assert "success" in driver.find_element(
-    By.ID, "first-name").get_attribute("class")
-
-assert "success" in driver.find_element(
-    By.ID, "last-name").get_attribute("class")
-
-assert "success" in driver.find_element(
-    By.ID, "address").get_attribute("class")
-
-assert "success" in driver.find_element(
-    By.ID, "e-mail").get_attribute("class")
-
-assert "success" in driver.find_element(
-    By.ID, "phone").get_attribute("class")
-
-assert "success" in driver.find_element(
-    By.ID, "city").get_attribute("class")
-
-assert "success" in driver.find_element(
-    By.ID, "country").get_attribute("class")
-
-assert "success" in driver.find_element(
-    By.ID, "job-position").get_attribute("class")
-
-assert "success" in driver.find_element(
-    By.ID, "company").get_attribute("class")
-
-
-driver.quit()
+    driver.quit()
--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -6,23 +6,24 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get(
-    "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+def test_calculator():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
-delay = driver.find_element(By.CSS_SELECTOR, "input#delay")
-delay.clear()
-delay.send_keys("45")
+    delay = driver.find_element(By.CSS_SELECTOR, "input#delay")
+    delay.clear()
+    delay.send_keys("45")
 
-driver.find_element(By.XPATH, "//span[text()='7']").click()
-driver.find_element(By.XPATH, "//span[text()='+']").click()
-driver.find_element(By.XPATH, "//span[text()='8']").click()
-driver.find_element(By.XPATH, "//span[text()='=']").click()
+    driver.find_element(By.XPATH, "//span[text()='7']").click()
+    driver.find_element(By.XPATH, "//span[text()='+']").click()
+    driver.find_element(By.XPATH, "//span[text()='8']").click()
+    driver.find_element(By.XPATH, "//span[text()='=']").click()
 
-WebDriverWait(driver, 55).until(
-    EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15"))
+    WebDriverWait(driver, 55).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15"))
 
-result = driver.find_element(By.CSS_SELECTOR, ".screen").text
-assert int(result) == 15
+    result = driver.find_element(By.CSS_SELECTOR, ".screen").text
+    assert int(result) == 15
 
-driver.quit()
+    driver.quit()
